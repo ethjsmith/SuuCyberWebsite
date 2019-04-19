035aa1579c3b4eec3cd0e4ac766aae5575fefe8a
Ethan Smith <ejsmith@suu.edu>
2019-04-19 08:59:31 -0600

    added something that I forgot about ?

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask,redirect
 from flask_basicauth import BasicAuth
 import subprocess, os, requests
 
-
+folder = os.path.dirname(os.path.realpath(__file__))
 # Path should be variable or something
-app = Flask(__name__,static_url_path='/home/ejsmith/Documents/suucyberwebsite')
+app = Flask(__name__,static_url_path='',static_folder=folder)
+
 
 @app.route('/')
 def main():
-	return app.send_static_file('index.html')
+	return app.send_static_file('/home/ejsmith/Documents/suucyberwebsite/index.html')
 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True,host='0.0.0.0')
